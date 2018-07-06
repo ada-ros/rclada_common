5e63af4121bd309bf5eaba313c4e4c3c1582d23a
Alejandro R. Mosteo <amosteo@unizar.es>
2018-07-06 11:11:43 +0200

    Moved to ament_cmake to make our life simpler, removed some cruft

--- cmake/ada_scope.cmake
+++ cmake/ada_scope.cmake
@@ -20,30 +20,29 @@ function(ada_end_package)
         message(STATUS "Using default Ada DefaultConfig.cmake.in since package doesn't provide one")
     endif()
 
-    file(MAKE_DIRECTORY ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/cmake)
+    #file(MAKE_DIRECTORY ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/cmake)
 
-    # The configuration file, at a minimum, should propagate the
-    # ADA_GPR_DIRS and ADA_GPRIMPORT_DIRS of the package
-    # (which are prepared in the ada_begin_package)
-    set(_conf_file ${PROJECT_NAME}Config.cmake)
+    # Being an ament_cmake package, we prepare a file for inclusion by ament_package
+    set(_conf_file ${PROJECT_NAME}-extras.cmake)
     configure_file(
             ${_conf_file_in}
             ${PROJECT_BINARY_DIR}/${_conf_file}
             @ONLY)
-    install(FILES
-            ${PROJECT_BINARY_DIR}/${_conf_file}
-            DESTINATION ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/cmake)
+
+    ament_package(CONFIG_EXTRAS ${PROJECT_BINARY_DIR}/${_conf_file})
+
+    # Remainder is now taken care of by ament
 
     # Version is dealt with with default CMake helpers
-    include(CMakePackageConfigHelpers)
-    write_basic_package_version_file(
-            ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/cmake/${PROJECT_NAME}ConfigVersion.cmake
-            COMPATIBILITY SameMajorVersion)
+    #include(CMakePackageConfigHelpers)
+    #write_basic_package_version_file(
+    #        ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/cmake/${PROJECT_NAME}ConfigVersion.cmake
+    #        COMPATIBILITY SameMajorVersion)
 #
 #    install(FILES
 #            ${PROJECT_BINARY_DIR}/${_cmake_conf_file}
 #            ${PROJECT_BINARY_DIR}/${_cmake_version_file}
 #            DESTINATION share/${PROJECT_NAME}/cmake)
 #
-    install(FILES package.xml DESTINATION share/${PROJECT_NAME})
+    #install(FILES package.xml DESTINATION share/${PROJECT_NAME})
 endfunction()
