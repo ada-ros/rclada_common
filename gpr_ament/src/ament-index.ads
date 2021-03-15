4f25863c469757bb577ab0e1d6d6208914d05690
Alejandro R. Mosteo <amosteo@unizar.es>
2021-03-15 14:32:04 +0100

    Clarify comment

--- gpr_ament/src/ament-index.ads
+++ gpr_ament/src/ament-index.ads
@@ -3,9 +3,10 @@ package Ament.Index is
    function Find_Library (Name  : String;
                           Local : Boolean := True)
                           return String;
-   --  Find a library installed under its expected "lib<package>__*.so" name.
-   --  If not found in its installation place and Local, look for it also under
-   --  current directory. (The generator gets called on the same build dir).
+   --  Find a library installed under its expected "libname__*.so" name. Name
+   --  is the full on-disk name. If not found in its installation place and
+   --  Local, look for it also under current directory. (The generator gets
+   --  called on the same build dir).
 
    function Find_Package (Name : String) return String;
    --  Finds where a package is installed. This will fail to locate the package
