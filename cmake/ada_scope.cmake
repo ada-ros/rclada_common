6aac686abfdfd734ad3a934c6700e79f152829bc
Alejandro R. Mosteo <amosteo@unizar.es>
2021-06-09 14:27:52 +0200

    Enable setting of GPR_PROJECT_PATH on demand

--- cmake/ada_scope.cmake
+++ cmake/ada_scope.cmake
@@ -18,6 +18,18 @@ function(ada_end_package)
 
     find_package(ament_cmake REQUIRED)
 
+    # Export the location of installed project files. This allows clients of rclada
+    # to be edited after sourcing the setup.bash. For development of rclada itself,
+    # this is not enough as this only points to installed headers. Use printenv_ada
+    # AFTER sourcing setup.bash in that case.
+    # NOTE: enabling this one is breaking package isolation, as any existing Ada 
+    # package will be reachable after sourcing, no matter if it is a dependency or
+    # not. For that reason, we leave this functionality as a convenience during for
+    # development with GNAT Studio that can be activated on request
+    if($ENV{RCLADA_EXPORT}) 
+        ament_environment_hooks("${ADA_RESOURCE_DIR}/ada_env_install.dsv.in")
+    endif()
+
     # When we close the Ada context we include at least the AdaConfig file that
     # builds the list of dirs for GPR, besides any other info the package exports
     ament_package(CONFIG_EXTRAS ${ADA_RESOURCE_DIR}/AdaConfig.cmake.in)
